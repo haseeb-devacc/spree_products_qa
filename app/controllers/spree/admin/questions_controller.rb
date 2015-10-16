b269b091dabfd36450d5882ab92b8f2f8f2c2563
haseeb-devacc <haseeb@devacc.com>
2015-10-16 12:36:27 +0500

    - question controller updated by removing create action - Validation on Answer content removed

--- app/controllers/spree/admin/questions_controller.rb
+++ app/controllers/spree/admin/questions_controller.rb
@@ -2,19 +2,7 @@ module Spree
   module Admin
     class QuestionsController < ResourceController
       belongs_to 'spree/product', find_by: :slug
-
-      def create
-        question = @product.questions.new(allowed_params)
-        question.user = spree_current_user
-        if question.save
-          flash[:notice] = t('question.sent')
-          redirect_to admin_product_questions_path(@product)
-        else
-          flash[:error] = question.errors.full_messages.to_sentence
-          redirect_to admin_product_questions_path(@product)
-        end
-      end
-
+      
       def index
         @collection = @collection.page params[:page]
       end
--- app/models/spree/answer.rb
+++ app/models/spree/answer.rb
@@ -1,8 +1,6 @@
 class Spree::Answer < ActiveRecord::Base
   belongs_to :question
 
-  # validates :content, presence: true
-
   after_create :send_email, if: ->() { SpreeProductsQa.send_email? }
 
   def send_email
